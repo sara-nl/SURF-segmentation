a1a1262df86617ea8d32e6d62255660b27430e46
Ruben Hekster <ruben.hekster@surfsara.nl>
2020-01-24 11:24:28 +0100

    Update run_prepro.sh

--- CAMELYON17_PREPROCESSING/run_prepro.sh
+++ CAMELYON17_PREPROCESSING/run_prepro.sh
@@ -2,7 +2,12 @@
 
 
 module load 2019
-module load Python/3.7.5-foss-2018b
+module load Python/3.6.6-foss-2018b
+
+export PATH=/home/$USER/examode/lib_deps/bin:$PATH
+export LD_LIBRARY_PATH=/home/$USER/examode/lib_deps/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/$USER/examode/lib_deps/lib:$LD_LIBRARY_PATH
+export CPATH=/home/$USER/examode/lib_deps/include:$CPATH
 
 source ~/.virtualenvs/openslide/bin/activate
 
